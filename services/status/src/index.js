ae2dcf1926af296f90fb448b0ff205fe1f969e9e
Chris Powell <chris.powell@charlottetilbury.com>
2020-05-25 11:52:30 +0100

    add release workflow

--- services/status/src/index.js
+++ services/status/src/index.js
@@ -1,5 +1,7 @@
+const { version } = require('../package.json');
+
 module.exports.handler = async() => ({ 
     statusCode: "200",
-    body: "{ \"status\":\"OK\" }",
-    headers: {} 
+    body: `{ "status":"OK", "version": "${version}" }`,
+    headers: { } 
 });
--- services/status/tests/main.test.js
+++ services/status/tests/main.test.js
@@ -1,10 +1,11 @@
 const { handler } = require('../src/index');
+const { version } = require('../package.json');
 
 describe('Status Endpoint', () => {
     it('Should normally return OK', async () => {
         expect(await handler()).toEqual({ 
             statusCode: "200",
-            body: "{ \"status\":\"OK\" }",
+            body: `{ "status":"OK", "version": "${version}" }`,
             headers: {} 
         })
     });
