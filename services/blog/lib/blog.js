b152a007c66ab83cda9092d31a16c72fb0cc8263
Chris Powell <chris.powell@charlottetilbury.com>
2020-06-23 21:33:24 +0100

    Switch to yarn workspaces

--- services/blog/lib/blog.js
+++ services/blog/lib/blog.js
@@ -3,12 +3,18 @@
 const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
 
 const listArticles = ({ contentful }) => async({page = 1, pageSize = 10}) => {
+
+    console.log("Getting entries")
     let { items } = await contentful.getEntries({
         skip: (page - 1) * pageSize,
         limit: pageSize,
         order: 'sys.updatedAt',
         content_type: 'blogPost'
     });
+
+    console.log(items.length + ' entries');
+
+    console.log(JSON.stringify(items, null, 4));
 
     return items.map(post => ({
         title: post.fields.title,
