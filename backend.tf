9066ae026cb3289d118612a3a9dd97ac1939ef40
Chris Powell <chris@chrisipowell.co.uk>
2021-02-13 12:54:16 +0000

    stop with the duping

--- backend.tf
+++ backend.tf
@@ -51,17 +51,6 @@ module "blog_list" {
   policies = [module.policies.cloudwatch_policy, aws_iam_policy.contentful_key_access.arn]
 }
 
-module "pages" {
-  source = "./terraform/modules/api/lambda_route"
-  api_gateway_id = module.api_gateway.api_gateway_id
-  api_gateway_execution_arn = module.api_gateway.api_gateway_execution_arn
-  method = "GET"
-  name = "pages"
-  handler = "lib/index.handler"
-  path = "/page"
-  policies = [module.policies.cloudwatch_policy, aws_iam_policy.contentful_key_access.arn]
-}
-
 module "page" {
   source = "./terraform/modules/api/lambda_route"
   api_gateway_id = module.api_gateway.api_gateway_id
