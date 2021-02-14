c74c838744ac4a78911beb52a76a4aed44ea3018
Chris Powell <chris@chrisipowell.co.uk>
2021-02-14 20:53:22 +0000

    add blog images to output and previews

--- services/pages/lib/index.js
+++ services/pages/lib/index.js
@@ -1,14 +1,12 @@
-// const AWSXRay = require('aws-xray-sdk-core');
-const { SecretsManager } = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core');
+const { SecretsManager } = AWSXRay.captureAWS(require('aws-sdk'));
 
-// AWSXRay.captureHTTPsGlobal(require('http'));
-// AWSXRay.captureHTTPsGlobal(require('https'));
-// AWSXRay.capturePromise();
+AWSXRay.captureHTTPsGlobal(require('http'));
+AWSXRay.captureHTTPsGlobal(require('https'));
+AWSXRay.capturePromise();
 
 const Contentful = require('contentful');
 const pages = require('./pages');
-
-const secretsManager = new SecretsManager({ region: 'eu-west-1' });
 
 const getSecret = () => new Promise((resolve, reject) => {   
     console.log("getting secret");
@@ -36,12 +34,11 @@ const setup = async () => {
 };
 
 module.exports.handler = async (event) => {
-    let { slug } = event.pathParameters
-    let { listPages } = await setup();
+    let { slug } = event.pathParmeters
+    let { listPages, getPage } = await setup();
 
-    let result = await listPages(slug);
-
-    console.log(JSON.stringify(result));
-
-    return result;
+    return { 
+        links : listPages(slug),
+        content: getPage(slug)
+    }
  }
