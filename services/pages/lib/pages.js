6b700c2efb59d804a343f4d60f3f701791fabe78
Chris Powell <chris@chrisipowell.co.uk>
2021-02-14 12:01:42 +0000

    Get pages and links

--- services/pages/lib/pages.js
+++ services/pages/lib/pages.js
@@ -1,42 +1,68 @@
 const { documentToHtmlString } = require('@contentful/rich-text-html-renderer');
 
-const listPages = ({ contentful }) => async (parentSlug = "") => {
-    let fieldQuery = 'fields.parent';
-    let fieldValue = parentSlug;
-
-    if (!parentSlug) {
-        fieldQuery = 'fields.parent[exists]',
-        fieldValue = false;
-    }
-
+const getPage = async (contentful, slug = "") => {
     let { items } = await contentful.getEntries({
         order: 'sys.updatedAt',
         content_type: 'page',
-        [fieldQuery]: fieldValue
+        'fields.slug[eq]': slug
     });
-
+   
     return items.map(item =>({
         slug: item.fields.slug,
-        title: items.fields.title
-    }));
-}
-
-const getPage = ({ contentful }) => async (slug) => {
-    let { items } = await contentful.getEntries({
-        order: 'sys.updatedAt',
-        content_type: 'page',
-        'fields.slug': slug
-    });
-
-    return items.map(item =>({
-        slug: item.fields.slug,
-        title: items.fields.title,
-        content: documentToHtmlString(items.content),
+        title: item.fields.title,
+        content: documentToHtmlString(item.fields.content),
         parent: {
-            slug: item.fields.parent.slug,
-            title: item.fields.title
+            slug: item.fields.parent?.slug,
+            title: item.fields.parent?.title
         }
     }));
 }
 
-module.exports = deps = {listPages: listPages(deps), getPage: getPage(deps)};
+const getRootLinks = async (contentful) => {
+    let { items } = await contentful.getEntries({
+        order: 'sys.updatedAt',
+        content_type: 'page',
+        'fields.parent[exists]': false
+    });
+
+    return items.map(item =>({
+        slug: item.fields.slug,
+        title: item.fields.title
+    }));
+}
+
+const getChildPageLinks = async (contentful, slug) => {
+    try {
+        let { items } = await contentful.getEntries({
+            content_type: 'page',
+            'fields.parent.sys.contentType.sys.id': 'page',
+            'fields.parent.fields.slug[eq]': slug
+        });
+
+        return items
+            .filter(item => item.fields.slug != slug)
+            .map(item =>({
+                slug: item.fields.slug,
+                title: item.fields.title
+            }));
+    } catch (err) {
+        console.error(err);
+    }
+
+    return {};
+}
+
+const listPages = ({ contentful }) => async (slug = "") => {
+    if (!slug) {
+        return { links: await getRootLinks(contentful), content: {} };
+    }
+
+    return { 
+        links: await getChildPageLinks(contentful, slug),
+        content: await getPage(contentful, slug)
+    }
+}
+
+
+
+module.exports = deps => ({listPages: listPages(deps)});
