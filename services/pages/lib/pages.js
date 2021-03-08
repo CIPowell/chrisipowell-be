84a3e2fee2ee4e31fd13db98df6bf1aac20c0d26
Chris Powell <chris@chrisipowell.co.uk>
2021-03-08 21:48:48 +0000

    fix page retrieval

--- services/pages/lib/pages.js
+++ services/pages/lib/pages.js
@@ -4,18 +4,18 @@ const getPage = async (contentful, slug = "") => {
     let { items } = await contentful.getEntries({
         order: 'sys.updatedAt',
         content_type: 'page',
-        'fields.slug[eq]': slug
+        'fields.slug': slug
     });
    
-    return items.map(item =>({
-        slug: item.fields.slug,
-        title: item.fields.title,
-        content: documentToHtmlString(item.fields.content),
+    return items.length ? {
+        slug: items[0].fields.slug,
+        title: items[0].fields.title,
+        content: documentToHtmlString(items[0].fields.content),
         parent: {
-            slug: item.fields.parent?.slug,
-            title: item.fields.parent?.title
+            slug: items[0].fields.parent?.fields.slug,
+            title: items[0].fields.parent?.fields.title
         }
-    }));
+    } : {};
 }
 
 const getRootLinks = async (contentful) => {
@@ -36,7 +36,7 @@ const getChildPageLinks = async (contentful, slug) => {
         let { items } = await contentful.getEntries({
             content_type: 'page',
             'fields.parent.sys.contentType.sys.id': 'page',
-            'fields.parent.fields.slug[eq]': slug
+            'fields.parent.fields.slug': slug
         });
 
         return items
